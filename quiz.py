a09a30e48b2716f249bb88265534239428826e87
leikcin-git <nickielpaulodangeles03@gmail.com>
2025-11-29 20:49:52 +0800

    Added Questions for quiz app

--- quiz.py
+++ quiz.py
@@ -9,8 +9,32 @@
 class Quiz:
     def __init__(self):
         self.questions = {
-            "Math": {
-                "What is 2 + 2?": "4",
-                "What is 5 * 6?": "30"
+            "Gaming": {
+                "What game features the phrase 'Finish Him'": "Mortal Kombat",
+                "Which company created Minecraft?": "Mojang",
+                "Which game has characters like Garen and Katarina?": "League of Legends",
+                "In what game do you battle using creatures called 'Pocket Monsters?": "Pokemon",
+                "What horror game series where you have to survive until 6 am?": "Five Nights at Freddy's",
             },
-        }
+
+
+            "Music": {
+                "Who is known as the 'King of Pop'?": "Michael Jackson",
+                "Who created the album 'BALLADS 1'?": "Joji",
+                "What genre of music known for heavy bass and beat drops?": "Dubstep",
+                "Which band released the album 'A Rush of Blood to the Head'?": "Coldplay",
+                "Who is the lead singer of the band 'Queen'?": "Freddie Mercury",
+
+
+            },
+
+            "IT Related Questions": {
+                "What does 'HTTP' stand for?": "HyperText Transfer Protocol",
+                "Who is the creator of java programming language?": "James Gosling",
+                "Which programming language is known for its snake logo?": "Python",
+                "What does 'GUI' stand for in computing?": "Graphical User Interface",
+                "What year was the World Wide Web invented?": "1989",
+            }
+        }    
+
+         
